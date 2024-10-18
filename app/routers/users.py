eaf5f847eb851228a28d88f7f64152082e8ab2e5
rinzio <163931041+rinzio@users.noreply.github.com>
2024-10-17 23:55:29 -0600

    Removed unnecessary methods from user model and view

--- app/routers/users.py
+++ app/routers/users.py
@@ -1,12 +1,9 @@
 from http import HTTPStatus as HTTP
 
-from fastapi import APIRouter, Depends, HTTPException
-
-from typing import Annotated, Any, List, Dict, Optional
+from fastapi import APIRouter, HTTPException
 
 from internal.db.repository import UserRepo
 from internal.db.models import BaseUserModel
-from app.dependencies import is_admin
 
 from internal.env import ENV
 from ..auth import AuthManager
@@ -36,67 +33,3 @@ async def post(user: BaseUserModel):
         return UserRepo.create(user.model_dump(by_alias=True, exclude=["id"])) # type: ignore
     except RuntimeError:
         raise HTTPException(status_code=HTTP.BAD_REQUEST, detail=f"User email already exists")
-
-@router.get(
-    "/",
-    response_description="List all users",
-    response_model=List[BaseUserModel],
-    status_code=HTTP.OK,
-    response_model_by_alias=False,
-)
-async def list(
-    _: Annotated[BaseUserModel, Depends(is_admin)],
-    id_: Optional[str] = None,
-    name: Optional[str] = None,
-    p_lastname: Optional[str] = None,
-    email: Optional[str] = None,
-    is_active: Optional[bool] = True,
-):
-    """
-    List all of the users data in the database.
-
-    The response is unpaginated and limited to 1000 results.
-    """
-    query: Dict[str, Any] = {k: v for k, v in {
-        "id": id_,
-        "name": name,
-        "p_lastname": p_lastname,
-        "email": email,
-        "is_active": is_active,
-    }.items() if v is not None}
-    return UserRepo.list(query)
-
-@router.get(
-    "/{id}",
-    response_description="Get a single user",
-    response_model=BaseUserModel,
-    status_code=HTTP.OK,
-    response_model_by_alias=False,
-)
-async def get(id: str, is_active: bool = True):
-    """
-    Get the record for a specific user, looked up by `id`.
-    """
-    if (user := UserRepo.get(id, is_active=is_active)) is None:
-        raise HTTPException(status_code=HTTP.NOT_FOUND, detail=f"Doctor {id} not found")
-    return user
-
-
-@router.delete(
-    "/{id}",
-    response_description="Delete a single user",
-    response_model=BaseUserModel,
-    status_code=HTTP.CREATED,
-    response_model_by_alias=False,
-)
-async def delete(
-    _: Annotated[BaseUserModel, Depends(is_admin)],
-    id: str
-):
-    """
-    Delete the record for a specific user, looked up by `id`.
-    """
-    to_delete = UserRepo.delete(id)
-    if to_delete is None:
-        raise HTTPException(status_code=HTTP.NOT_FOUND, detail=f"User {id} not found")
-    return to_delete
--- internal/db/repository/user.py
+++ internal/db/repository/user.py
@@ -1,10 +1,8 @@
-from datetime import datetime
-from typing import Any, Dict, List, Optional
+from typing import Dict
 
 from pydantic import EmailStr
-from pymongo.collection import Collection, ReturnDocument
+from pymongo.collection import Collection
 from pymongo.errors import DuplicateKeyError
-from bson import ObjectId
 
 from internal.db.models.id_ import PyObjectId
 
@@ -31,54 +29,3 @@ class UserRepo(Repository):
             return BaseUserModel.model_validate(cls.get(neo.inserted_id))
         except DuplicateKeyError:
             raise RuntimeError("Duplicated key, user not created")
-
-    @classmethod
-    def get(cls, id: str | None = None, email: EmailStr | None = None, is_active: bool = True) -> Optional[BaseUserModel]:
-        query: Dict[str, Any] = {"is_active": is_active}
-        if id is not None:
-            query["_id"] = ObjectId(id)
-        if email is not None:
-            query["email"] = email
-    
-        if (data := cls.__sui.find_one(query)):
-            return BaseUserModel.model_validate(data)
-        
-    @classmethod  # Name alias
-    def get_by(cls, id: str | None = None, email: EmailStr | None = None, *args, **kwargs):
-        return cls.get(id, email, *args, **kwargs)
-
-    @classmethod
-    def list(cls, queryset: Optional[Dict[str, Any]] = None, limit: int = 1000) -> List[BaseUserModel]:
-        if not queryset:
-            queryset = {"is_active": True}
-        data = cls.__sui.find(queryset).to_list(limit)
-        return [BaseUserModel.model_validate(obj) for obj in data]
-
-    @classmethod
-    def delete(cls, id: str | None = None, email: EmailStr | None = None, soft: bool = True) -> Optional[BaseUserModel]:
-        if soft:
-            changeset = {
-                "updated_at": datetime.now(),
-                "is_active": False
-            }
-            return cls.update(id, email, changeset)
-        raise RuntimeError(f"Hard delete not defined for doctors")
-
-    @classmethod
-    def update(cls, id: str | None = None, email: EmailStr | None = None, changeset: Dict = {}) -> Optional[BaseUserModel]:
-        changeset["updated_at"] = datetime.now()
-        query: Dict[str, Any] = {"is_active": True} # Only active users can be updated
-        if id is not None:
-            query["_id"] = ObjectId(id)
-        if email is not None:
-            query["email"] = email
-        
-        if changeset:
-            update_result = cls.__sui.find_one_and_update(
-                query,
-                {"$set": changeset},
-                return_document=ReturnDocument.AFTER,
-            )
-            if update_result is not None:
-                return BaseUserModel.model_validate(update_result)
-        return cls.get(id, email)
