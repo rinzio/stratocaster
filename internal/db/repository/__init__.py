8c632c8db069de2bfda1b7d3cb70bfd49d8a9979
rinzio <163931041+rinzio@users.noreply.github.com>
2024-10-17 20:49:11 -0600

    Added readonly repository

--- internal/db/repository/__init__.py
+++ internal/db/repository/__init__.py
@@ -1,3 +1,3 @@
 from internal.db.repository.patient import Patients
 from internal.db.repository.doctor import Doctors
-from internal.db.repository.repository import Repository
+from internal.db.repository.repository import Repository, ReadOnlyRepository
